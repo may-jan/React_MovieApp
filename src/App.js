2148259933c76034a039b344c93dc51a66279d58
Jaeeun <jaeeun9862@gmail.com>
2023-09-18 17:36:00 +0900

    Clean Up

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+
+function Hello() {
+  useEffect(() => {
+    console.log('hi :)');
+    return () => console.log('bye :(');
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
+  const [showing, setShowing] = useState(false);
 
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log('I run all the time');
-
-  useEffect(() => {
-    console.log('- - - CALL THE API - - -');
-  }, []);
-
-  useEffect(() => {
-    console.log(`keyword 변경될 때마다 실행 ${keyword}`);
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log(`counter 변경될 때마다 실행 ${counter}`);
-  }, [counter]);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
-    <div className='App'>
-      <div>
-        <input
-          value={keyword}
-          onChange={onChange}
-          type='text'
-          placeholder='Search here'
-        />
-        <h1>Hello React {counter}</h1>
-        <button onClick={onClick}>Click!</button>
-      </div>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
